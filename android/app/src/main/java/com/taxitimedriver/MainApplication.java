439e71e66a90357669235963f4106f882ee6fd19
Xaus <lvlr.xaus@gmail.com>
2018-03-29 13:55:12 +0430

    set fonts for android

--- android/app/src/main/java/com/taxitimedriver/MainApplication.java
+++ android/app/src/main/java/com/taxitimedriver/MainApplication.java
@@ -3,6 +3,7 @@ package com.taxitimedriver;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import net.zubricky.AndroidKeyboardAdjust.AndroidKeyboardAdjustPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new VectorIconsPackage(),
-          new AndroidKeyboardAdjustPackage()
+            new AndroidKeyboardAdjustPackage(),
+          new VectorIconsPackage()
 
       );
     }
